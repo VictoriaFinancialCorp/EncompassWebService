772b39ea6d3f54058854f6fdee1785c60a00d1b2
Calvin Thanh <calvin@victoriafinancial.local>
2017-03-15 16:58:20 -0700

    backup path

--- api/controllers/BackupController.js
+++ api/controllers/BackupController.js
@@ -2,6 +2,9 @@ module.exports = {
 	backup: function (req, res) {
 		var action = req.param('action');
 		if(action == 'mers'){
+			if(sails.config.connections.mySql.password == ''){
+				return res.serverError("db password required for login");
+			}
 			mySqlBackup('mers',sails.config.connections.mySql.user, sails.config.connections.mySql.password);
 
 			return res.ok('done');
@@ -17,13 +20,14 @@ function mySqlBackup(db, login, pw){
 	const exec = require('child_process').exec;
 	var mysqldump = sails.config.connections.mySql.path + "mysqldump.exe";
 	var fileName =  db + "_bk" + moment().format('YYYYMMDD-HHmmss') + '.sql';
-	var query = mysqldump + ' -u '+ login + ' -p ' + pw + ' ' + db + ' > backup\\' + fileName ;
+	var query = mysqldump + ' -u '+ login + ' -p' + pw + ' ' + db + ' > backup\\' + fileName ;
 	//console.log(query);
 	exec(query, (error, stdout, stderr) => {
 	  if (error) {
 	    console.error(`exec error: ${error}`);
 	    return;
 	  }
+
 	  console.log(`stdout: ${stdout}`);
 	  console.log(`stderr: ${stderr}`);
 	});
