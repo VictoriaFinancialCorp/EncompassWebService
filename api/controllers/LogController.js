fb2d2fa60c9b089b08b4b8a60dcfffb396bdf345
Calvin Thanh <calvin@victoriafinancial.local>
2017-04-12 16:16:22 -0700

    server log file

--- api/controllers/LogController.js
+++ api/controllers/LogController.js
@@ -7,13 +7,37 @@
 
 module.exports = {
 	show: function(req, res){
-		Log.find({
-			limit:50,
-			sort:'createdAt DESC'
-		}).exec(function(err, logs){
-			if (err) return res.json(err);
-			return res.view('logs', {logs});
-		});
+		const fs = require('fs');
+		var path = sails.config.appPath + "/assets/myLog.log";
+
+		//server log file if exists
+		if(fs.existsSync(path)){
+			var lineReader = require('readline').createInterface({
+				input: fs.createReadStream(path)
+			});
+
+			var output ="<pre>";
+			lineReader.on('line', function (line) {
+				output += line +"\n" ;
+			});
+
+			lineReader.on('close', function(){
+				output +="</pre>";
+				return res.ok(output);
+			})
+		}
+		//else show logs from db
+		else{
+			Log.find({
+				limit:50,
+				sort:'createdAt DESC'
+			}).exec(function(err, logs){
+				if (err) return res.json(err);
+				return res.view('logs', {logs});
+			});
+		}
+
+
 
 	}
 
